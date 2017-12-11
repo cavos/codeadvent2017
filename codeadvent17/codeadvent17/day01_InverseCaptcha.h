edb83b9e7f39e19f4435df9c81c6796f8daa6bf1
Krzysztof Kwieciński <cavos@users.noreply.github.com>
2017-12-11 09:56:57 +0100

    day01 part 2

--- codeadvent17/codeadvent17/day01_InverseCaptcha.h
+++ codeadvent17/codeadvent17/day01_InverseCaptcha.h
@@ -10,7 +10,9 @@ public:
 	~InverseCaptcha();
 
 	static unsigned compute(std::istream& inputStream);
+	static unsigned compute_pt2(std::istream& inputStream);
 
 	static std::stringstream testinput;
+	static std::stringstream testinput_pt2;
 };
 
